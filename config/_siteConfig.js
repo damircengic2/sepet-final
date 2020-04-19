e8a9ff517f514f4bb75e5444c2da11e17c46d2f0
Damir Cengic <info@damircengic.me>
2020-04-19 20:44:09 +0200

    Change of logo

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -12,7 +12,7 @@ export default {
   siteName: 'Amphibians Blog',
   tagline: 'The latest from our Scuba diving team.',
   featureImage: '/uploads/home-hero.jpg',
-  logo: 'logo-component', // 'logo-component', // or '/logo.svg' for regular image
+  logo: 'logo.svg', // 'logo-component', // or '/logo.svg' for regular image
   googleAnalytics: {
     on: true,
     id: process.env.GOOGLE_ANALYTICS_ID
